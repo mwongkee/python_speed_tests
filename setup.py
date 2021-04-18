52f59a8cffe0604e92a760e5abd9f9a764d8f716
Matthew Wongkee <matt.wongkee@gmail.com>
2021-04-18 01:07:11 -0400

    profiling a few things

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, Extension
+import os
+
+
+os.environ["CC"] = "gcc-9"
+
+ext = Extension(
+      'c_extension',
+      sources = ['c_extension.c'],
+      extra_compile_args=['-fopenmp'],
+      extra_link_args=['-lgomp'])
+
+
+setup(name='c_extension',
+       version='1.0',
+       description='This is a demo package',
+       ext_modules=[ext])
